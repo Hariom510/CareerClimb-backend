ad8e576e229c8c3d2d4d1bd273e887a76af0ae7f
Hariom Kumar <hariom.star.hk@gmail.com>
2023-04-29 16:10:54 +0530

    changes made

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const formRoute = require("./routes/forms");
 const path = require("path");
 const PORT = process.env.PORT || 5000
 
+const cors  = require("cors");
+
+const corsOptions ={
+    origin:'https://my-application-form.netlify.app',    
+    credentials:true, 
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 dotenv.config(); 
 app.use(express.json());
 app.use(
