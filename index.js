7b108373ffb75922389ce2d4fc9f2e380488e7f3
Hariom Kumar <hariom.star.hk@gmail.com>
2023-07-30 17:07:22 +0530

    second commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 const cors  = require("cors");
 
 const corsOptions ={
-    origin:'https://my-application-form.netlify.app',  
+    origin:'https://careerclimb.netlify.app/',  
     // origin:'http://localhost:3000',    
     credentials:true, 
     optionSuccessStatus:200
